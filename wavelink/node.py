b0ac5016f922e7f2da8b41d07b6e10593028e228
EvieePy <evieepy@gmail.com>
2019-01-04 19:12:26 +1000

    Fix import logging

--- wavelink/node.py
+++ wavelink/node.py
@@ -20,6 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+import logging
 from discord.ext import commands
 from typing import Optional, Union
 from urllib.parse import quote
