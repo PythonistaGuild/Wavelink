1351a9876444da0e14c36c497e5341c667b157e4
EvieePy <evieepy@gmail.com>
2019-01-04 19:52:01 +1000

    Add events to imports

--- wavelink/__init__.py
+++ wavelink/__init__.py
@@ -6,6 +6,7 @@ __version__ = '0.0.1a'
 
 from .client import Client
 from .errors import *
+from .events import *
 from .player import Player, Track
 from .node import Node
 from .websocket import WebSocket
