0cfbbb241d9fa9c7d73e7f412d86c9af88300000
EvieePy <evieepy@gmail.com>
2019-01-05 14:11:00 +1000

    Update to stats.

--- wavelink/stats.py
+++ wavelink/stats.py
@@ -1,5 +1,46 @@
+"""MIT License
+
+Copyright (c) 2018 Luke & William
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
+
+class Penalty:
+    def __init__(self, stats):
+        self.player_penalty = stats.playing_players
+        self.cpu_penalty = 1.05 ** (100 * stats.system_load) * 10 - 10
+        self.null_frame_penalty = 0
+        self.deficit_frame_penalty = 0
+
+        if stats.frames_nulled is not -1:
+            self.null_frame_penalty = (1.03 ** (500 * (stats.frames_nulled / 3000))) * 300 - 300
+            self.null_frame_penalty *= 2
+
+        if stats.frames_deficit is not -1:
+            self.deficit_frame_penalty = (1.03 ** (500 * (stats.frames_deficit / 3000))) * 600 - 600
+
+        self.total = self.player_penalty + self.cpu_penalty + self.null_frame_penalty + self.deficit_frame_penalty
+
+
 class Stats:
-    def __init__(self, data):
+    def __init__(self, node, data):
+        self._node = node
 
         self.uptime = data['uptime']
 
