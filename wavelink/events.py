8758c204edebf630eaa59e721c7eb27d84ea7e00
Eviee <evieepy@gmail.com>
2020-02-04 06:34:45 +1100

    Added a base event class for typings.

--- wavelink/events.py
+++ wavelink/events.py
@@ -1,4 +1,43 @@
-class TrackEnd:
+"""MIT License
+
+Copyright (c) 2019-2020 PythonistaGuild
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from .player import Player
+from .player import Track
+
+
+__all__ = ('WavelinkEvent', 'TrackEnd', 'TrackException', 'TrackStuck')
+
+
+class WavelinkEvent:
+    """Base Wavelink event class. All events derive from this class."""
+
+    __slots__ = ('player', 'track')
+
+    def __init__(self, player: Player, track: Track):
+        self.player = player
+        self.track = track
+
+
+class TrackEnd(WavelinkEvent):
     """Event dispatched on TrackEnd.
 
     :ivar player: The :class:`wavelink.player.Player` associated with the event.
@@ -6,18 +45,17 @@ class TrackEnd:
     :ivar reason: The reason the TrackEnd event was dispatched.
     """
 
-    __slots__ = ('player', 'track', 'reason')
+    __slots__ = ('reason', )
 
-    def __init__(self, player, track, reason: str):
-        self.player = player
-        self.track = track
+    def __init__(self, player: Player, track: Track, reason: str):
+        super().__init__(player, track)
         self.reason = reason
 
     def __str__(self):
         return 'TrackEnd'
 
 
-class TrackException:
+class TrackException(WavelinkEvent):
     """Event dispatched on TrackException.
 
     :ivar player: The :class:`wavelink.player.Player` associated with the event.
@@ -25,18 +63,17 @@ class TrackException:
     :ivar error: The error associated with the event.
     """
 
-    __slots__ = ('player', 'track', 'error')
+    __slots__ = ('error', )
 
-    def __init__(self, player, track, error: Exception):
-        self.player = player
-        self.track = track
+    def __init__(self, player: Player, track: Track, error: Exception):
+        super().__init__(player, track)
         self.error = error
 
     def __str__(self):
         return 'TrackException'
 
 
-class TrackStuck:
+class TrackStuck(WavelinkEvent):
     """Event dispatched on TrackStuck.
 
     :ivar player: The :class:`wavelink.player.Player` associated with the event.
@@ -44,11 +81,10 @@ class TrackStuck:
     :ivar threshold: The threshold associated with the event.
     """
 
-    __slots__ = ('player', 'track', 'threshold')
+    __slots__ = ('threshold', )
 
-    def __init__(self, player, track, threshold: int):
-        self.player = player
-        self.track = track
+    def __init__(self, player: Player, track: Track, threshold: int):
+        super().__init__(player, track)
         self.threshold = threshold
 
     def __str__(self):
