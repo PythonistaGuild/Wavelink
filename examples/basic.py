f9e3684f4674fce625c2aa4dc6cd78108ed0b53e
Adure <aranwit1@hotmail.com>
2020-04-24 14:27:23 +1000

    change port in basic example

--- examples/basic.py
+++ examples/basic.py
@@ -30,7 +30,7 @@ class Music(commands.Cog):
         # Initiate our nodes. For this example we will use one server.
         # Region should be a discord.py guild.region e.g sydney or us_central (Though this is not technically required)
         await self.bot.wavelink.initiate_node(host='0.0.0.0',
-                                              port=80,
+                                              port=2333,
                                               rest_uri='http://0.0.0.0:2333',
                                               password='youshallnotpass',
                                               identifier='TEST',
