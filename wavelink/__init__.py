78b0601375edac8b4becb5439dab49f47174a5d2
EvieePy <evieepy@gmail.com>
2020-05-03 01:42:21 +1000

    Bump version (0, 6, 3)

--- wavelink/__init__.py
+++ wavelink/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'WaveLink'
 __author__ = 'EvieePy'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019-2020 (c) PythonistaGuild'
-__version__ = '0.6.2'
+__version__ = '0.6.3'
 
 from .client import Client
 from .errors import *
