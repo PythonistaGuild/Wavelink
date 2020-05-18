b249997c46789973527e76374022830b2dbfdd69
Jens Reidel <jens.reidel@gmail.com>
2020-05-18 12:28:32 +0200

    Bump version to 0.8.0

--- wavelink/__init__.py
+++ wavelink/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'WaveLink'
 __author__ = 'EvieePy'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019-2020 (c) PythonistaGuild'
-__version__ = '0.7.1'
+__version__ = '0.8.0'
 
 from .client import Client
 from .errors import *
