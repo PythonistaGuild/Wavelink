cb096c4e256bf91c52457695ac6fadb2eb75e9bd
Eviee <evieepy@gmail.com>
2022-02-14 15:11:06 +1000

    Remove discord.py from enums.py

--- wavelink/enums.py
+++ wavelink/enums.py
@@ -23,13 +23,7 @@ SOFTWARE.
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-
-if TYPE_CHECKING:
-    from enum import Enum
-else:
-    from discord import Enum
+from enum import Enum
 
 __all__ = (
     "ErrorSeverity",
