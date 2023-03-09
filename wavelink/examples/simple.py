be8ea425d645b7a2d0b501124507318a904fbee3
EvieePy <evieepy@gmail.com>
2023-03-09 23:47:52 +1000

    Add a small sample of examples.

---
+++ wavelink/examples/simple.py
@@ -0,0 +1,70 @@
+"""
+MIT License
+
+Copyright (c) 2019-Present PythonistaGuild
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import discord
+import wavelink
+from discord.ext import commands
+
+
+class Bot(commands.Bot):
+
+    def __init__(self) -> None:
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        super().__init__(intents=intents, command_prefix='?')
+
+    async def on_ready(self) -> None:
+        print(f'Logged in {self.user} | {self.user.id}')
+
+    async def setup_hook(self) -> None:
+        # Wavelink 2.0 has made connecting Nodes easier... Simply create each Node
+        # and pass it to NodePool.connect with the client/bot.
+        node: wavelink.Node = wavelink.Node(uri='http://localhost:2333', password='youshallnotpass')
+        await wavelink.NodePool.connect(client=self, nodes=[node])
+
+
+bot = Bot()
+
+
+@bot.command()
+async def play(ctx: commands.Context, *, search: str) -> None:
+    """Simple play command."""
+
+    if not ctx.voice_client:
+        vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+    else:
+        vc: wavelink.Player = ctx.voice_client
+
+    track = await wavelink.YouTubeTrack.search(search, return_first=True)
+    await vc.play(track)
+
+
+@bot.command()
+async def disconnect(ctx: commands.Context) -> None:
+    """Simple disconnect command.
+
+    This command assumes there is a currently connected Player.
+    """
+    vc: wavelink.Player = ctx.voice_client
+    await vc.disconnect()
