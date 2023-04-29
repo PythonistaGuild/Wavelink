9c72f72fa8d3fa9058885b1e75767b14c3ae3375
Buğrahan Arslan <34966090+Nowe5@users.noreply.github.com>
2023-04-29 22:54:33 +0300

    Should get item after checking self.loop_all and self.is_empty
    It was looping only one, to loop endlessly item should be gotten after checking self.loop_all and self.is_empty condition

--- wavelink/queue.py
+++ wavelink/queue.py
@@ -261,11 +261,11 @@ class Queue(BaseQueue):
         if self.loop and self._loaded:
             return self._loaded
 
-        item = super()._get()
         if self.loop_all and self.is_empty:
             self._queue.extend(self.history._queue)
             self.history.clear()
-
+        item = super()._get()
+        
         self._loaded = item
         self.history.put(item)
 
