42046f09cbd60fa8a5e8c16cc3a90ac4a7ce232a
EvieePy <evieepy@gmail.com>
2023-05-20 18:48:04 +1000

    Add `DiscordVoiceCloseType` enum.

--- wavelink/enums.py
+++ wavelink/enums.py
@@ -23,7 +23,7 @@ SOFTWARE.
 """
 from discord.enums import Enum
 
-__all__ = ('NodeStatus', 'TrackSource', 'LoadType', 'TrackEventType')
+__all__ = ('NodeStatus', 'TrackSource', 'LoadType', 'TrackEventType', 'DiscordVoiceCloseType')
 
 
 class NodeStatus(Enum):
@@ -55,3 +55,19 @@ class TrackEventType(Enum):
 
     START = 'TrackStartEvent'
     END = 'TrackEndEvent'
+
+
+class DiscordVoiceCloseType(Enum):
+    CLOSE_NORMAL = 1000  # Not Discord but standard websocket
+    UNKNOWN_OPCODE = 4001
+    FAILED_DECODE_PAYLOAD = 4002
+    NOT_AUTHENTICATED = 4003
+    AUTHENTICATION_FAILED = 4004
+    ALREADY_AUTHENTICATED = 4005
+    SESSION_INVALID = 4006
+    SESSION_TIMEOUT = 4009
+    SERVER_NOT_FOUND = 4011
+    UNKNOWN_PROTOCOL = 4012
+    DISCONNECTED = 4014
+    VOICE_SERVER_CRASHED = 4015
+    UNKNOWN_ENCRYPTION_MODE = 4016
