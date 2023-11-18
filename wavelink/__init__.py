7f4bcac4c6dcdf6ee1842db36922407fa77d184c
EvieePy <evieepy@gmail.com>
2023-11-18 23:44:07 +1000

    Bump Version (RC2)

--- wavelink/__init__.py
+++ wavelink/__init__.py
@@ -25,7 +25,7 @@ __title__ = "WaveLink"
 __author__ = "PythonistaGuild, EvieePy"
 __license__ = "MIT"
 __copyright__ = "Copyright 2019-Present (c) PythonistaGuild, EvieePy"
-__version__ = "3.0.0rc1"
+__version__ = "3.0.0rc2"
 
 
 from .enums import *
