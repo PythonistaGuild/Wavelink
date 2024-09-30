4adb9794c7fc9cd19f4f57d443d4b48bcb6c187d
Mysty <evieepy@gmail.com>
2024-09-30 21:43:06 +1000

    Fix/filter values (#326)
    * Add baseFilter class and remove None values from filter payloads.
    
    * Bump version

--- wavelink/__init__.py
+++ wavelink/__init__.py
@@ -26,7 +26,7 @@ __title__ = "WaveLink"
 __author__ = "PythonistaGuild, EvieePy"
 __license__ = "MIT"
 __copyright__ = "Copyright 2019-Present (c) PythonistaGuild, EvieePy"
-__version__ = "3.4.1"
+__version__ = "3.4.2"
 
 
 from .enums import *
